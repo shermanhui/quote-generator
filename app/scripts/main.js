12faa3879522d97b6b7f9f83ba5d29792e1b9b42
shui91 <sherman.sy.hui@gmail.com>
2016-01-05 20:56:30 -0800

    chore: in progress, using getJSON

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(){
 	var generator = document.getElementById("generate");
+	var request = new Request('http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?', {
+		method: 'GET',
+		mode: 'no-cors',
+		redirect: 'follow'
+	});
 
 	generator.onclick = function(){
-		console.log("clicked!");
-	}
-})
+		$.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?', 'jsonp');
+
+	};
+});
